c8fe19e58ce52ed945a04795305e9184d9b64fd9
Jan Cumps <jcumps@gmail.com>
2025-04-20 22:50:38 +0200

    debug sm irq

--- source/stepper_impl.cpp
+++ source/stepper_impl.cpp
@@ -31,17 +31,17 @@ bool stepper_callback_controller::interrupt_manager::register_stepper(stepper_ca
 }
 
 void stepper_callback_controller::interrupt_manager::interrupt_handler(PIO pio) {
-    printf("=======================\n");
-    for(auto a: steppers_) {
-        printf("stepper registered: %d\n", a);
-    }
-    printf("-----------------------\n");
+    // printf("=======================\n");
+    // for(auto a: steppers_) {
+    //     printf("stepper registered: %d\n", a);
+    // }
+    // printf("-----------------------\n");
     uint sm = pio_irq_util::sm_from_interrupt(pio->irq, stepper_PIO_IRQ_DONE);
-    printf("sm: %d\n", sm);
+    // printf("sm: %d\n", sm);
     size_t idx = index_for(pio, sm);
-    printf("idx: %d\n", idx);
+    // printf("idx: %d\n", idx);
     stepper_callback_controller *stepper =  steppers_[idx];
-    printf("stepper found: %d\n", stepper);
+    // printf("stepper found: %d\n", stepper);
     if (stepper != nullptr) {
         stepper -> handler();
     }
